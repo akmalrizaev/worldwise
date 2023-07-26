479fd41fa6d7cffeb6e6275cfb333a250c80992f
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-07-26 15:21:04 +0500

    Programming Navigation with useNavigate

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import styles from './Button.module.css';
+function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
